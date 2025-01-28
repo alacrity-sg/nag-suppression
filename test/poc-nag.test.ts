a051d21a1bcb0d028fb710f2ee1ab8c47558f5cb
Bock Yang <bockyanggoh@gmail.com>
2025-01-28 12:28:04 +0800

    Initial commit

---
+++ test/poc-nag.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PocNag from '../lib/poc-nag-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/poc-nag-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PocNag.PocNagStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
